49143811e0bf58a29ab1c0d3315819788781ef76
rraadd88 <rohanadandage@gmail.com>
2020-05-12 19:25:36 -0400

    update

--- DMS_DionneEtal2020.py
+++ DMS_DionneEtal2020.py
@@ -1,6 +1,11 @@
+# load required libraries
+# (see ./requirements.txt for the required python packages.)
 from rohan.dandage.io_dict import read_dict
 from rohan.dandage.align.demultiplex import run_demupliplex
 from rohan.dandage.align.deepseq_dms import get_mutmat
-cfg=read_dict('all_pooled_global_input_cfg.yml')
+# load configuration file containing parameters for the analysis 
+cfg=read_dict('configuration.yml')
+# demultiplex samples
 run_demupliplex(cfg)
+# obtain sample-wise mutation matrices
 get_mutmat(cfg)
--- SingleParticleTracking_DionneEtal2020.sh
+++ SingleParticleTracking_DionneEtal2020.sh
@@ -1,10 +1,10 @@
 # Instructions for the installation of `htsimaging`: https://github.com/rraadd88/htsimaging#installation
-# Instructions for the usage of `htsimaging`: run `python endocytosis.py run-trials -h`
+# Instructions for the usage of `htsimaging`: `python endocytosis.py run-trials -h`
 # Inputs:
-# $project_dir : contains the subdirectories containing time-lapse images for individual trials.
-# $bright_fn_marker: In each subdirectory containing images for individual trials the filename of bright-field image contains a unique label. 
+# $project_dir : contains the subdirectories containing time-lapse images for individual trials. In each subdirectory containing images for individual trials the filename of bright-field image contains an unique label.
+# $bright_fn_marker: unique label in the filename of bright-field images.
 
 # Activate the Anaconda environment 
 conda activate htsimaging
-# Execute the command for the analysis of images 
+# Execute the command for the analysis of time-lapse images 
 python endocytosis.py run-trials $project_dir $bright_fn_marker
